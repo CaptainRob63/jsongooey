68963f9d523158637817b886731731392eaa6943
banana <fotyek.robert63@gmail.com>
2025-11-16 04:14:36 +0100

    lexer start

--- src/main/java/nhf/parser/Token.java
+++ src/main/java/nhf/parser/Token.java
@@ -3,4 +3,36 @@ package nhf.parser;
 public class Token {
     private TokenType type;
 
+
+    private String lexeme;
+    private Object literal;
+    private final int line;
+
+    public Token(TokenType type, String lexeme, Object literal, int line) {
+        this.type = type;
+        this.lexeme = lexeme;
+        this.literal = literal;
+        this.line = line;
+    }
+
+    public TokenType getType() {
+        return type;
+    }
+
+    public String getLexeme() {
+        return lexeme;
+    }
+
+    public Object getLiteral() {
+        return literal;
+    }
+
+    public int getLine() {
+        return line;
+    }
+
+    @Override
+    public String toString() {
+        return type + " " + lexeme + " " + literal;
+    }
 }
